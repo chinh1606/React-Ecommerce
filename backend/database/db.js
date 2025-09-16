0bd728876765a85aabc8fdbbca19358189be6dd7
nguyentachinh2003@gmail.com <nguyentachinh2003@gmail.com>
2025-09-16 21:00:19 +0700

    update config database

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,11 +1,15 @@
-require('dotenv').config()
-const mongoose=require("mongoose")
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-exports.connectToDB=async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log('connected to DB');
-    } catch (error) {
-        console.log(error);
-    }
-}
+exports.connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Thay đổi thời gian chờ
+    });
+    console.log("connected to DB");
+  } catch (error) {
+    console.log(error);
+  }
+};
